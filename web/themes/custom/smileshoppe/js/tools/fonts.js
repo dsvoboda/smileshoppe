3a46236fb8ffe31a4825b09d6ef04534fdf35671
dsvoboda <dave.svoboda@vertexvis.com>
2020-07-14 10:55:02 -0500

    custom theme folder

---
+++ web/themes/custom/smileshoppe/js/tools/fonts.js
@@ -0,0 +1,13 @@
+(function() {
+	WebFontConfig = {
+		custom: { families: ['Roboto:n3,n4,n9'], urls: ['https://fonts.googleapis.com/css?family=Roboto:300,400,900&display=swap'] },
+	};
+})();
+(function() {
+	var wf = document.createElement('script');
+	wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
