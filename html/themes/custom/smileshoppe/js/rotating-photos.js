dc84e46737070994a4aefe6bfb55c8624624dcd7
dsvoboda <dave.svoboda@vertexvis.com>
2020-07-16 20:53:27 -0500

    layout for rotating photos

--- html/themes/custom/smileshoppe/js/rotating-photos.js
+++ html/themes/custom/smileshoppe/js/rotating-photos.js
@@ -3,12 +3,27 @@
 	
 	Drupal.behaviors.smileshoppe_rotating_photos = {
 		attach: function(context, settings) {
-			setupPhotos();
+			setupPhotos($('#block-views-block-rotating-photos-block-1'));
 		}
 	}
 	
 	function setupPhotos($block) {
-		alert('rotating photos setup called');
+		$block.once('rotating-photos').each(function(i) {
+			var $wrapper = $block.find('.item-list-wrapper');
+			var $item_list = $wrapper.find('.item-list');
+			var $items = $item_list.find('img');
+			$items.each(function(a) {
+				var $item = $(this);
+				if(a == 0) {
+					var $clone = $item.clone();
+					$clone.appendTo($wrapper);
+					$clone.addClass('sentinel');
+				}
+				$item.addClass('init').appendTo($wrapper);
+				$item_list.remove();
+			});
+			$block.addClass('rotate-init');
+		});
 	}
 	
 }(jQuery));
