400704e87240e906c1e43580f89fd44bc9aa3363
dsvoboda <dave.svoboda@vertexvis.com>
2020-07-18 12:57:29 -0500

    scope

--- html/themes/custom/smileshoppe/js/gallery.js
+++ html/themes/custom/smileshoppe/js/gallery.js
@@ -6,27 +6,28 @@
 			$('#block-views-block-gallery-gallery').once('gallery-setup').each(function(i) {
 				setupGallery($(this));
 			});
+			
+			function setupGallery($block) {
+				var $items = $('.item-list .type-gallery-image', context);
+				var $wrapper = $('.item-list-wrapper', context);
+				$items.each(function(a) {
+					imageSetup($(this));
+					$(this).appendTo($wrapper);
+				});
+				$('.item-list',context).remove();
+				$block.addClass('ready');
+			}
+			
+			function imageSetup($elem) {
+				var $before = $elem.find('.before');
+				var $after = $elem.find('.after');
+				$before.css('background-image','url(' + $before.find('img').attr('src') + ')');
+				$after.css('background-image','url(' + $after.find('img').attr('src') + ')');
+				$before.empty();
+				$after.empty();
+				$elem.addClass('init');
+			}
 		}
-	}
-	function setupGallery($block) {
-		var $items = $('.item-list .type-gallery-image', context);
-		var $wrapper = $('.item-list-wrapper', context);
-		$items.each(function(a) {
-			imageSetup($(this));
-			$(this).appendTo($wrapper);
-		});
-		$('.item-list',context).remove();
-		$block.addClass('ready');
-	}
-	
-	function imageSetup($elem) {
-		var $before = $elem.find('.before');
-		var $after = $elem.find('.after');
-		$before.css('background-image','url(' + $before.find('img').attr('src') + ')');
-		$after.css('background-image','url(' + $after.find('img').attr('src') + ')');
-		$before.empty();
-		$after.empty();
-		$elem.addClass('init');
 	}
 	
 }(jQuery));
