085ae88dfbfca484004133ed9cf274b90ede0deb
dsvoboda <dave.svoboda@vertexvis.com>
2020-07-18 14:33:14 -0500

    promise

--- html/themes/custom/smileshoppe/js/gallery.js
+++ html/themes/custom/smileshoppe/js/gallery.js
@@ -7,52 +7,56 @@
 		attach: function(context, settings) {
 			$(context).find('.item-list-wrapper').once('gallery-setup').each(function(i) {
 				$images = [];
-				setupGallery($(this));
-				setHeight($(this));
+				$gallery = setupGallery($(this));
+				setHeight($gallery);
 			});
 		}
 	}
 	
 	function setupGallery($wrapper) {
-		var $items = $wrapper.find('.type-gallery-image');
-		$items.each(function(a) {
-			imageSetup($(this));
-			$(this).appendTo($wrapper);
+		let gallery = new Promise(function(resolve,reject) {
+			var $items = $wrapper.find('.type-gallery-image');
+			$items.each(function(a) {
+				imageSetup($(this));
+				$(this).appendTo($wrapper);
+			});
+			$wrapper.find('.item-list').remove();
+			$wrapper.append($('<div />', {
+				'class' : 'next'
+			}));
+			$wrapper.append($('<div />', {
+				'class' : 'prev'
+			}));
+			$wrapper.attr('data-index',$wrapper.find(':first-child').attr('data-id'));
+			$wrapper.find('[data-id="' + $wrapper.attr('data-index') + '"]').addClass('active');
+			$wrapper.find('.next').click(function(e) {
+				e.preventDefault();
+				$wrapper.find('.type-gallery-image').removeClass('active');
+				var next = parseInt($images.indexOf($wrapper.attr('data-index')) + 1);
+				if(next < $images.length) {
+					$wrapper.find('[data-id="' + $images[next] + '"]').addClass('active');
+				} else {
+					$wrapper.find('[data-id="' + $images[0] + '"]').addClass('active');
+				}
+				$wrapper.attr('data-index',$wrapper.find('.active').attr('data-id'));
+				setHeight($wrapper);
+			});
+			$wrapper.find('.prev').click(function(e) {
+				e.preventDefault();
+				$wrapper.find('.type-gallery-image').removeClass('active');
+				var prev = parseInt($images.indexOf($wrapper.attr('data-index')) - 1);
+				if(prev >= 0) {
+					$wrapper.find('[data-id="' + $images[prev] + '"]').addClass('active');
+				} else {
+					$wrapper.find('[data-id="' + $images[parseInt($images.length - 1)] + '"]').addClass('active');
+				}
+				$wrapper.attr('data-index',$wrapper.find('.active').attr('data-id'));
+				setHeight($wrapper);
+			});
+			$('#block-views-block-gallery-gallery').addClass('ready');
+			resolve($wrapper);
 		});
-		$wrapper.find('.item-list').remove();
-		$wrapper.append($('<div />', {
-			'class' : 'next'
-		}));
-		$wrapper.append($('<div />', {
-			'class' : 'prev'
-		}));
-		$wrapper.attr('data-index',$wrapper.find(':first-child').attr('data-id'));
-		$wrapper.find('[data-id="' + $wrapper.attr('data-index') + '"]').addClass('active');
-		$wrapper.find('.next').click(function(e) {
-			e.preventDefault();
-			$wrapper.find('.type-gallery-image').removeClass('active');
-			var next = parseInt($images.indexOf($wrapper.attr('data-index')) + 1);
-			if(next < $images.length) {
-				$wrapper.find('[data-id="' + $images[next] + '"]').addClass('active');
-			} else {
-				$wrapper.find('[data-id="' + $images[0] + '"]').addClass('active');
-			}
-			$wrapper.attr('data-index',$wrapper.find('.active').attr('data-id'));
-			setHeight($wrapper);
-		});
-		$wrapper.find('.prev').click(function(e) {
-			e.preventDefault();
-			$wrapper.find('.type-gallery-image').removeClass('active');
-			var prev = parseInt($images.indexOf($wrapper.attr('data-index')) - 1);
-			if(prev >= 0) {
-				$wrapper.find('[data-id="' + $images[prev] + '"]').addClass('active');
-			} else {
-				$wrapper.find('[data-id="' + $images[parseInt($images.length - 1)] + '"]').addClass('active');
-			}
-			$wrapper.attr('data-index',$wrapper.find('.active').attr('data-id'));
-			setHeight($wrapper);
-		});
-		$('#block-views-block-gallery-gallery').addClass('ready');
+		return gallery;
 	}
 	
 	function setHeight($wrapper) {
