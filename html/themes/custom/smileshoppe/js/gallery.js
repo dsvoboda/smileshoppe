48ed6440704746b380f056ff577b4341cf3e4916
dsvoboda <dave.svoboda@vertexvis.com>
2020-07-18 13:47:55 -0500

    next button functionality

--- html/themes/custom/smileshoppe/js/gallery.js
+++ html/themes/custom/smileshoppe/js/gallery.js
@@ -1,9 +1,12 @@
 (function ($) {
 	'use strict';
 	
+	var $images = [];
+	
 	Drupal.behaviors.smileshoppe_gallery = {
 		attach: function(context, settings) {
 			$(context).find('.item-list-wrapper').once('gallery-setup').each(function(i) {
+				$images = [];
 				setupGallery($(this));
 			});
 		}
@@ -16,6 +19,21 @@
 			$(this).appendTo($wrapper);
 		});
 		$wrapper.find('.item-list').remove();
+		$wrapper.attr('data-index',$wrapper.find(':first-child').attr('data-id'));
+		$wrapper.find('[data-index="' + $wrapper.attr('data-index') + '"]').addClass('active');
+		$wrapper.find('.next').click(function(e) {
+			e.preventDefault();
+			$wrapper.find('.type-gallery-image').removeClass('active');
+			var next = parseInt($images.indexOf($wrapper.attr('data-index')) + 1);
+			if(next <= $images.length) {
+				$wrapper.find('[data-index="' + next + '"]').addClass('active');
+			} else {
+				$wrapper.find('[data-index="' + $wrapper.attr('data-index') + '"]').addClass('active');
+			}
+		});
+		$wrapper.find('.prev').click(function(e) {
+			e.preventDefault();
+		});
 		$('#block-views-block-gallery-gallery').addClass('ready');
 	}
 	
@@ -26,6 +44,7 @@
 		$after.css('background-image','url(' + $after.find('img').attr('src') + ')');
 		$before.empty();
 		$after.empty();
+		$images.push($elem.attr('data-id'));
 		$elem.addClass('init');
 	}
 	
